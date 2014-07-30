3725b7686aa49d418946ae7b8cb370f603fc5dc1
Carlos Agarie <carlos.agarie@gmail.com>
2014-07-29 23:24:31 -0400

    VCS prompt in zshrc

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-DOTFILES=(ackrc gemrc irbrc pryrc rvmrc vim vimrc zshenv zshrc gitconfig gitignore_global Rprofile)
+DOTFILES=(ackrc gemrc irbrc pryrc rvmrc vimrc zshenv zshrc gitconfig gitignore_global Rprofile)
 
 # Make symlinks for all the files in the `rc` repository.
 for rc in $DOTFILES; do
--- zshrc
+++ zshrc
@@ -58,6 +58,9 @@ setopt prompt_subst
 PROMPT='%B%F{magenta}%c%B%F{green}${vcs_info_msg_0_}%B%F{magenta} %{$reset_color%}λ '
 RPROMPT='%{$fg_bold[yellow]%}[%?]%{$reset_color%}'
 
+autoload -U add-zsh-hook
+add-zsh-hook precmd  theme_precmd
+
 ### Git aliases
 alias gst='git status -sb'
 

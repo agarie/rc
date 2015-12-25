9ffd6a2b09b1b7074dbc8c6d0dfca075eebcd831
Carlos Agarie <carlos.agarie@gmail.com>
2015-12-25 19:41:19 -0200

    i3_config: Add configuration for i3wm

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env zsh
 
-DOTFILES=(ackrc gemrc irbrc pryrc vimrc zprofile zshenv zshrc gitconfig gitignore_global Rprofile tmux.conf)
+git submodule update --init
 
 # Make symlinks for all the files in the `rc` repository.
-for rc in $DOTFILES; do
-  rm ~/.$rc
+for rc in ackrc gemrc irbrc pryrc vimrc zprofile zshenv zshrc gitconfig gitignore_global Rprofile; do
+  rm -f ~/.$rc
   ln -s ~/rc/$rc ~/.$rc
 done
 
+# Vim configuration.
+rm -f ~/.vim
 ln -s ~/rc/vim ~/.vim
+
+# i3wm configuration.
+mkdir -p ~/.config/i3
+ln -s ~/rc/i3_config ~/.config/i3/config

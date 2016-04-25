3bf34e4466ecb2e946f09d021d9656ec84dddf94
Carlos Agarie <carlos.agarie@gmail.com>
2016-04-25 15:58:51 -0300

    setup.sh: copy rc files to ~/ + install everything

--- setup.sh
+++ setup.sh
@@ -1,36 +1,61 @@
 #!/usr/bin/env zsh
 
-# This script installs the programming languages and libraries I usually use for work.
+# Dotfiles per operating system.
+COMMON=(ackrb gemrc irbrc pryrc vimrc zprofile zshenv zshrc gitconfig gitignore_global Rprofile)
+LINUX=(i3status.conf Xresources)
+
+# There are some vim bundles added as submodules.
+git submodule update --init
+
+# Symlinks for all operating systems.
+for f in $COMMON; do
+  rm -f ~/.$f
+  ln -s $f ~/.$f
+done
 
 case $(uname -s) in
   Darwin)
+
+    # TODO: Check if Homebrew is already installed.
     # Install Homebrew.
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update
 
-    ;;
+  ;;
+  # Should change depending on Ubuntu or Arch Linux...
   Linux)
-    # Update local repositories.
-    sudo apt-get update
+    for f in $LINUX; do
+      rm -f ~/.$f
+      ln -s $f ~/.$f
+    done
 
-
-    ;;
+  ;;
 esac
 
+# Vim configuration.
+rm -f ~/.vim
+ln -s ~/rc/vim ~/.vim
+
+# i3wm configuration.
+mkdir -p ~/.config/i3
+ln -s ~/rc/i3_config ~/.config/i3/config
+
+#
+# Software installation.
+#
 
 # BLAS and LAPACK.
-
 # - install openblas/atlas.
 
-
 # Ruby.
-
 # - install ruby-install, chruby
-# - install ruby 1.9.3, 2.0, 2.1, 2.2
+# - install latest ruby
 # - for each ruby, install: pry, bundler, smarter_csv, nokogiri, iruby
 # - eventually NMatrix, statsample, statsample-{glm,timeseries}, distribution, integration,
 #   minimization, plotrb, nyaplot
 
+sudo ./install/ruby-install.sh
+sudo ./install/chruby.sh
 
 # Python.
 
@@ -40,18 +65,15 @@ esac
 #   csvkit, pep8, jedi
 # - create a virtualenv "datasci" with --use-site-packages
 
-
 # C/C++.
 
 # - install gcc-4.9/g++-4.9
 # - C:
 # - C++: libboost (latest stable)
 
-
 # Julia.
 
 # - install latest stable version.
-
 
 # R.
 

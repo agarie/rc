d20fd4d8b86cdb07624b9bb34ac6a9c689711427
Carlos Agarie <carlos.agarie@gmail.com>
2016-05-13 15:40:33 -0300

    Use correct dotfile locations when symlinking
    The most important change in this commit is:
    
      -RC=$(dirname $0)
      +RC=$(dirname $0:A)
    
    So, if I call the script as `./setup.sh` from $HOME/rc, the RC variable will be,
    correctly, `$HOME/rc`.

--- setup.sh
+++ setup.sh
@@ -3,15 +3,18 @@
 # Dotfiles per operating system.
 COMMON=(ackrc gitconfig gitignore_global irbrc pryrc vim vimrc zprofile zshenv zshrc)
 LINUX=(i3status.conf Xdefaults tmux.conf)
-RC=$(dirname $0)
+RC=$(dirname $0:A)
 
 # Backup existing dotfile, then create a symlink.
 function link_dotfile {
   if [[ $(readlink -f ~/.$1) != $(readlink -f $RC/$1) ]]; then
-    echo "Linking $1..."
+    echo ">> Linking $1..."
     ln -isT $RC/$1 ~/.$1
   fi
 }
+
+# TODO: Check if the tools I use daily are available.
+# vim, zsh, tmux, ruby, git
 
 # There are some vim bundles added as submodules.
 echo "Updating submodules..."
@@ -24,8 +27,6 @@ done
 
 case $(uname -s) in
   Darwin)
-    echo "Mac OS X detected."
-
     # Install Homebrew.
     if whence brew > /dev/null; then
       "Installing Homebrew..."
@@ -39,36 +40,24 @@ case $(uname -s) in
     cabal update
     pip install virtualenv virtualenvwrapper
   ;;
-  # Should change depending on Ubuntu or Arch Linux...
+  # TODO: Should change depending on Ubuntu or Arch Linux...
   Linux)
     for f in $LINUX; do
       link_dotfile $f
     done
 
-    # i3wm configuration.
-    mkdir -p ~/.config/i3
-    ln -s $RC/i3_config ~/.config/i3/config
-
-    if whence ruby-install > /dev/null; then
-      echo "Installing ruby-install..."
-      sudo ./install/ruby-install.sh > /dev/null 2>&1
+    if [[ $(readlink -f $RC/i3_config) != $(readlink -f ~/.config/i3/config) ]]; then
+      echo ">> Linking i3 configuration..."
+      mkdir -p ~/.config/i3
+      ln -isT $RC/i3_config ~/.config/i3/config
     fi
-
-    if whence chruby > /dev/null; then
-      echo "Installing chruby..."
-      sudo ./install/chruby.sh > /dev/null 2>&1
-    fi
-
-    # Install GHC, Cabal, etc.
-    # Install Python and Python3.
   ;;
 esac
 
 # Install latest version of MRI, enable it and install basic gems.
-ruby-install -L # Update Ruby versions list.
-ruby-install ruby 2.3
-chruby 2.3
-gem install bundler pry
-
-# C/C++.
-# - install latest gcc/clang.
+if (whence chruby > /dev/null) && (whence ruby-install > /dev/null); then
+  ruby-install -L # Update Ruby versions list.
+  ruby-install ruby 2.3
+  chruby 2.3
+  gem install bundler pry
+fi

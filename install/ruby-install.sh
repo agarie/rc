573db07cf1f12e56f29913be80e054eaf94ed355
Carlos Agarie <carlos.agarie@gmail.com>
2020-06-29 23:54:29 -0300

    Remove custom install scripts
    Installation of these packages -- ruby-install, chruby, vim, and
    subsample -- should always be done via a package manager.

--- install/ruby-install.sh
+++
@@ -1,7 +0,0 @@
-mkdir ~/Downloads
-cd ~/Downloads
-
-wget -O ruby-install-0.7.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
-tar -xzvf ruby-install-0.7.0.tar.gz
-cd ruby-install-0.7.0/
-sudo make install

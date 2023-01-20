71a683ed7d9dd6ff0a9cf8f6ad30daba094b1fb2
Carlos Agarie <carlos.agarie@tarmac.io>
2023-01-20 14:55:35 -0300

    Converted init.vim to init.lua
    I rewrote most options using the NeoVim API (`vim.opt`) and mostly kept the rest intact inside `vim.cmd` calls.
    
    Next step is to rewrite commands, autocmds, etc, in Lua.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,96 +1,93 @@
 -- Carlos Agarie's vimrc
 
--- Still using old .vim directory with bunch of stuff.
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-
 -- General {{{
-set nocompatible
-set hidden
-set magic                         " z0mg magic
-set backspace=indent,eol,start    " Backspace can delete stuff in insert mode.
-set clipboard=unnamedplus         " Enable system clipboard.
-set number                        " Show line numbers.
-set relativenumber
-set cursorline                    " Highlight current line.
-set nobackup                      " LOL BACKUPS
-set noswapfile
-set foldopen-=block               " Don't open folds when moving around with paragraph text objects ({ and }).
-set foldcolumn=1                  " Show fold information in the gutter.
-set foldmethod=expr               " Use Treesitter for code folding.
-set foldexpr=nvim_treesitter#foldexpr()
+vim.opt.hidden = true
+vim.opt.backspace = { "indent", "eol", "start" } -- Backspace can delete stuff in insert mode.
+vim.opt.clipboard = "unnamedplus"  -- Enable system clipboard.
+vim.opt.number = true                        -- Show line numbers.
+vim.opt.relativenumber = true
+vim.opt.cursorline = true                    -- Highlight current line.
+vim.opt.backup = false                      -- LOL BACKUPS
+vim.opt.swapfile = false
+vim.opt.foldopen:remove("block")               -- Don't open folds when moving around with paragraph text objects ({ and }).
+vim.opt.foldcolumn = "1"                  -- Show fold information in the gutter.
+vim.opt.foldmethod = "expr"               -- Use Treesitter for code folding.
+vim.cmd("set foldexpr=nvim_treesitter#foldexpr()")
 
 -- Indentation
-set autoindent                    " Copies current line indentation when creating a new line.
-set tabstop=8                     " Hard tabs (eww) are equivalent to 8 spaces.
-set softtabstop=2                 " Tab key, 1 tab = 2 spaces.
-set shiftwidth=2                  " Autoindent, 1 tab = 2 spaces.
-set shiftround                    " Indentation is set to multiples of shiftwidth.
-set expandtab                     " Never use hard tabs. To insert one, use CTRL-V <Tab>.
+vim.opt.autoindent = true                    -- Copies current line indentation when creating a new line.
+vim.opt.tabstop = 8                     -- Hard tabs (eww) are equivalent to 8 spaces.
+vim.opt.softtabstop = 2                 -- Tab key, 1 tab = 2 spaces.
+vim.opt.shiftwidth = 2                  -- Autoindent, 1 tab = 2 spaces.
+vim.opt.shiftround = true                    -- Indentation is set to multiples of shiftwidth.
+vim.opt.expandtab = true                     -- Never use hard tabs. To insert one, use CTRL-V <Tab>.
 
 -- Searching
-set hlsearch                      " Highlight matches.
-set incsearch                     " Incremental search.
-set ignorecase                    " Ignore case...
-set smartcase                     " ... except when they contain at least one capital letter.
+vim.opt.hlsearch = true                      -- Highlight matches.
+vim.opt.incsearch = true                     -- Incremental search.
+vim.opt.ignorecase = true                    -- Ignore case...
+vim.opt.smartcase = true                     -- ... except when they contain at least one capital letter.
 
 -- Open new split panes to right and bottom.
-set splitbelow
-set splitright
+vim.cmd([[
+	set splitbelow
+	set splitright
 
-colorscheme molokai
+	colorscheme molokai
 
--- Window height.
-set winheight=30
-
+" Window height.
+	set winheight=30
+]])
 -- }}}
 
 -- Plugins — installation {{{
-call plug#begin()
-Plug 'JuliaEditorSupport/julia-vim'
-Plug 'LaTeX-Box-Team/LaTeX-Box'
-Plug 'Raimondi/delimitMate'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'mattn/emmet-vim'
-Plug 'neovim/nvim-lspconfig'
-Plug 'numToStr/Comment.nvim'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-Plug 'onsails/lspkind.nvim'
-Plug 'rlue/vim-fold-rspec'
-Plug 'stephpy/vim-php-cs-fixer'
-Plug 'tpope/vim-endwise'
-call plug#end()
+vim.cmd([[
+	call plug#begin()
+	Plug 'JuliaEditorSupport/julia-vim'
+	Plug 'LaTeX-Box-Team/LaTeX-Box'
+	Plug 'Raimondi/delimitMate'
+	Plug 'hrsh7th/nvim-cmp'
+	Plug 'hrsh7th/cmp-nvim-lsp'
+	Plug 'mattn/emmet-vim'
+	Plug 'neovim/nvim-lspconfig'
+	Plug 'numToStr/Comment.nvim'
+	Plug 'nvim-lua/plenary.nvim'
+	Plug 'nvim-telescope/telescope.nvim'
+	Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+	Plug 'onsails/lspkind.nvim'
+	Plug 'rlue/vim-fold-rspec'
+	Plug 'stephpy/vim-php-cs-fixer'
+	Plug 'tpope/vim-endwise'
+	call plug#end()
+]])
 
 -- Pathogen and bundles.
---""""""""""""""""""""""
 -- TODO: Use `vim-plug` instead of Pathogen.
 
-execute pathogen#infect()
-execute pathogen#helptags()
+vim.cmd([[
+	execute pathogen#infect()
+	execute pathogen#helptags()
+	]])
 
 -- }}}
 
 -- Plugin config — Lua {{{
 
 -- Telescope
-lua << EOF
 require('telescope').setup {
   defaults = {
     file_ignore_patterns = {".git", "node_modules"}
   }
 }
-EOF
 
-nnoremap <C-P> <cmd>Telescope find_files hidden=true<cr>
-nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-nnoremap <leader>fb <cmd>Telescope buffers<cr>
-nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+vim.cmd([[
+	nnoremap <C-P> <cmd>Telescope find_files hidden=true<cr>
+	nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+	nnoremap <leader>fb <cmd>Telescope buffers<cr>
+	nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+]])
 
 -- LSP
-lua << EOF
 require'nvim-treesitter.configs'.setup{}
 require'lspconfig'.tsserver.setup{}
 require'lspconfig'.cssls.setup{}
@@ -132,15 +129,13 @@ local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-EOF
 
 -- }}}
 
 -- Plugin config — VimScript {{{
 
 -- airline
---""""""""
-
+vim.cmd([[
 let g:airline_powerline_fonts = 1
 
 if !exists('g:airline_symbols')
@@ -148,14 +143,15 @@ if !exists('g:airline_symbols')
 endif
 let g:airline_symbols.colnr = " ℅:"
 
--- vim-markdown
+" vim-markdown
 let g:markdown_folding = 1
-
+]])
 -- }}}
 
 -- vimwiki {{{
 
 -- Configuration for my personal notes.
+vim.cmd([[
 let g:vimwiki_list = [{
       \ 'path': $HOME . '/Code/Personal/notes',
       \ 'path_html': $HOME . '/Code/Personal/notes/notes_html',
@@ -168,88 +164,107 @@ let g:vimwiki_list = [{
       \ 'auto_diary_index': 1,
       \ 'auto_tags': 1}]
 
--- Let folding be defined by another plugin.
+" Let folding be defined by another plugin.
 let g:vimwiki_folding = 'expr'
 
--- Do not create a 'temporary wiki' whenever I open a *.md file.
--- A temporary wiki is a mechanism by which vimwiki can create an anonymous
--- wiki if it can't find one defined in your environment.
+" Do not create a 'temporary wiki' whenever I open a *.md file.
+" A temporary wiki is a mechanism by which vimwiki can create an anonymous
+" wiki if it can't find one defined in your environment.
 let g:vimwiki_global_ext = 0
-
+]])
 -- }}}
 
 -- Remaps {{{
 
 -- Leader is used in general remaps, localleader for filetype remaps.
+vim.cmd([[
 let mapleader = ","
 let maplocalleader = ";"
+]])
 
 -- Use ,k to break into a new line.
-nnoremap <Leader>k i<cr><esc>k$
+vim.cmd([[nnoremap <Leader>k i<cr><esc>k$]])
 
 -- Use ,e to edit files in the current directory.
-cnoremap %% <C-R>=expand('%:h').'/'<cr>
-map <Leader>e :edit %%<cr>
+vim.cmd([[cnoremap %% <C-R>=expand('%:h').'/'<cr>]])
+vim.cmd([[map <Leader>e :edit %%<cr>]])
 
 -- Use ^J, ^K, ^L and ^H to navigate between splits.
+vim.cmd([[
 nnoremap <C-J> <C-W><C-J>
 nnoremap <C-K> <C-W><C-K>
 nnoremap <C-L> <C-W><C-L>
 nnoremap <C-H> <C-W><C-H>
+]])
 
 -- Use ,- and ,= to navigate between tabs.
+vim.cmd([[
 noremap <Leader>- :tabprevious<CR>
 noremap <Leader>= :tabnext<CR>
+]])
 
 -- Use ,[ and ,] to navigate between buffers.
+vim.cmd([[
 noremap <Leader>[ :bnext<CR>
 noremap <Leader>] :blast<CR>
+]])
 
 -- "in line" (entire line sans white-space; cursor at beginning--ie, ^)
+vim.cmd([[
 xnoremap <silent> iL :<c-u>normal! g_v^<cr>
 onoremap <silent> iL :<c-u>normal! g_v^<cr>
+]])
 
 -- "around line" (entire line sans trailing newline; cursor at beginning--ie, 0)
+vim.cmd([[
 xnoremap <silent> aL :<c-u>normal! $v0<cr>
 onoremap <silent> aL :<c-u>normal! $v0<cr>
+]])
 
 -- Exit insert mode.
-inoremap jk <esc>
+vim.cmd([[inoremap jk <esc>]])
 
 -- Save file path for c/p.
-noremap \l :let @+ = expand('%')<cr>
-nnoremap <Leader>/ :Ggrep! <c-r>=expand('<cword>')<CR>
+vim.cmd([[noremap \l :let @+ = expand('%')<cr>]])
+vim.cmd([[nnoremap <Leader>/ :Ggrep! <c-r>=expand('<cword>')<CR>]])
 
+vim.cmd([[
 noremap \\ :wa<return>
 noremap ZA :wa<cr>:qa<cr>
 noremap ZC :qa!<cr>
+]])
 
 -- Highlight words without moving cursor.
-noremap <Leader><Leader>h *#
+vim.cmd([[noremap <Leader><Leader>h *#]])
 
 -- Search and replace word under cursor.
-nnoremap <Leader>sr :%s/\<<C-r><C-w>\>/
+vim.cmd([[nnoremap <Leader>sr :%s/\<<C-r><C-w>\>/]])
 
 -- Clear highlight.
-nnoremap <Leader>n :noh<cr>
+vim.cmd([[nnoremap <Leader>n :noh<cr>]])
 
 -- Yank until the end of line.
-noremap Y y$
+vim.cmd([[noremap Y y$]])
 
 -- }}}
 
 --" Language configuration {{{
 
+vim.cmd([[
 filetype plugin indent on
 syntax on
 syntax enable
+]])
 
 -- Indentation for various languages.
+vim.cmd([[
 autocmd Filetype python,java,scala,javascript,php,c,c++,julia set sw=4 sts=4 et
 autocmd FileType ruby,eruby,yaml,html,sass,cucumber set sw=2 sts=2 et
+]])
 
-au BufNewFile,BufRead PKGBUILD set ft=sh
+vim.cmd([[au BufNewFile,BufRead PKGBUILD set ft=sh]])
 
+vim.cmd([[
 augroup vimscript
   autocmd!
   autocmd FileType vim setlocal foldmethod=marker
@@ -259,27 +274,29 @@ augroup markdown
   autocmd!
   autocmd FileType markdown setlocal foldlevel=1
 augroup END
+]])
 
 -- }}}
 
 -- Extra autocommands and functions {{{
 
 -- Remove any trailing whitespace that is in the file.
-autocmd BufRead,BufWrite * if ! &bin | silent! %s/\s\+$//ge | endif
+vim.cmd([[autocmd BufRead,BufWrite * if ! &bin | silent! %s/\s\+$//ge | endif]])
 
 -- Introduce current date as yyyy-mm-dd.
-autocmd Filetype markdown,vimwiki nnoremap <buffer> <LocalLeader>dt :.!today --hyphen<cr>
+vim.cmd([[autocmd Filetype markdown,vimwiki nnoremap <buffer> <LocalLeader>dt :.!today --hyphen<cr>]])
 
 -- Introduce current time as HH:MM.
-autocmd Filetype markdown,vimwiki nnoremap <buffer> <LocalLeader>dh :.!now<cr>
+vim.cmd([[autocmd Filetype markdown,vimwiki nnoremap <buffer> <LocalLeader>dh :.!now<cr>]])
 
 -- Remove content under current header in vimwiki.
 -- This mapping is recursive as it is using the vimwiki header text-object.
 -- See :help vimwiki-text-objects
-autocmd Filetype vimwiki nmap <buffer> <LocalLeader>xh [[jdih
+vim.cmd([[autocmd Filetype vimwiki nmap <buffer> <LocalLeader>xh [[jdih]])
 
 -- Scratch buffer.
 -- https://www.gilesorr.com/blog/vim-tips-23-scratch-buffer.html
+vim.cmd([[
 function Newscratch()
     execute 'tabnew '
     setlocal buftype=nofile
@@ -287,6 +304,7 @@ function Newscratch()
     setlocal noswapfile
 endfunction
 command! Ns call Newscratch()
+]])
 
 -- }}}
 

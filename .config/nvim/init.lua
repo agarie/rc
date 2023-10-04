91cdc3b6e9046d0707120974467be3f7b9d25f5c
Carlos Agarie <carlos.agarie@tarmac.io>
2023-10-04 17:16:53 -0300

    nvim: add symbols to LSP output

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -105,30 +105,32 @@ require('lspconfig').solargraph.setup{}
 local lspkind = require('lspkind')
 local cmp = require('cmp')
 cmp.setup {
-      mapping = {
-         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-         ["<C-e>"] = cmp.mapping.close(),
-         ["<c-y>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-         },
+  mapping = {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<c-y>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+  },
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = 'symbol',
+      maxwidth = 50,
+      with_text = true,
+      menu = {
+        buffer   = "[buf]",
+        nvim_lsp = "[LSP]",
+        path     = "[path]",
       },
-      formatting = {
-         format = lspkind.cmp_format {
-            with_text = true,
-            menu = {
-               buffer   = "[buf]",
-               nvim_lsp = "[LSP]",
-               path     = "[path]",
-            },
-         },
-      },
-      sources = {
-         { name = "nvim_lsp"},
-         { name = "path" },
-         { name = "buffer" , keyword_length = 5},
-      }
+    },
+  },
+  sources = {
+    { name = "nvim_lsp"},
+    { name = "path" },
+    { name = "buffer" , keyword_length = 5},
+  }
 }
 
 local opts = { noremap=true, silent=true }

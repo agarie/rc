cf6949acf3a4b7d0b2d475ef432166652420bcce
Carlos Agarie <carlos.agarie@tarmac.io>
2023-10-05 17:30:29 -0300

    nvim: add mappings for LSP code completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -105,7 +105,9 @@ require('lspconfig').solargraph.setup{}
 local lspkind = require('lspkind')
 local cmp = require('cmp')
 cmp.setup {
-  mapping = {
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-e>"] = cmp.mapping.close(),

e68f329e922e4057f78bbf1e0763d78c1fbd5a42
Carlos Agarie <carlos.agarie@gmail.com>
2024-02-20 17:42:34 -0300

    polybar: show on both monitors

--- .config/polybar/start.sh
+++ .config/polybar/start.sh
@@ -8,6 +8,14 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 echo '--- start' | tee -a /tmp/polybar.log
 
+# Taken from: https://github.com/polybar/polybar/issues/763
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload batou &
+  done
+else
+  polybar --reload batou &
+fi
 polybar batou &
 
 echo "Bar launched at $(date)" | tee -a /tmp/polybar.log

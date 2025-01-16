3a0accd0ca207306077190d36a813ae8a37d9fb6
Carlos Agarie <carlos.agarie@tarmac.io>
2025-01-16 10:41:26 -0300

    nvim: rewritten vimscript snippets into lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,13 +89,8 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]resume' })
-
-vim.cmd([[
-	nnoremap <C-P> <cmd>Telescope find_files hidden=true<cr>
-	nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-	nnoremap <leader>fb <cmd>Telescope buffers<cr>
-	nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-]])
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[F]ind [B]uffers'})
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = 'Find files'})
 -- }}}
 
 -- Treesitter configuration {{{

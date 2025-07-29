963ae01052480f3b2c32b1ffa14e8379bd032010
Carlos Agarie <carlos.agarie@gmail.com>
2025-07-29 17:18:18 -0300

    nvim: lol LSP keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -189,7 +189,27 @@ cmp.setup {
   }
 }
 
--- TODO: There are other LSP capabilities that need keymaps.
+-- TODO: Check if there are other LSP capabilities that need keymaps.
+-- NOTE: Default keymaps in Neovim 0.11:
+--
+-- https://neovim.io/doc/user/lsp.html#_defaults
+--
+-- GLOBAL DEFAULTS
+-- "grn" is mapped in Normal mode to vim.lsp.buf.rename()
+-- "gra" is mapped in Normal and Visual mode to vim.lsp.buf.code_action()
+-- "grr" is mapped in Normal mode to vim.lsp.buf.references()
+-- "gri" is mapped in Normal mode to vim.lsp.buf.implementation()
+-- "grt" is mapped in Normal mode to vim.lsp.buf.type_definition()
+-- "gO" is mapped in Normal mode to vim.lsp.buf.document_symbol()
+-- CTRL-S is mapped in Insert mode to vim.lsp.buf.signature_help()
+-- "an" and "in" are mapped in Visual mode to outer and inner incremental selections, respectively, using vim.lsp.buf.selection_range()
+--
+-- BUFFER-LOCAL DEFAULTS
+-- 'omnifunc' is set to vim.lsp.omnifunc(), use i_CTRL-X_CTRL-O to trigger completion.
+-- 'tagfunc' is set to vim.lsp.tagfunc(). This enables features like go-to-definition, :tjump, and keymaps like CTRL-], CTRL-W_], CTRL-W_} to utilize the language server.
+-- 'formatexpr' is set to vim.lsp.formatexpr(), so you can format lines via gq if the language server supports it.
+-- To opt out of this use gw instead of gq, or clear 'formatexpr' on LspAttach.
+-- K is mapped to vim.lsp.buf.hover() unless 'keywordprg' is customized or a custom keymap for K exists.
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc='LSP: [G]o to [D]efinition' })
 vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc='LSP: [G]o to [R]eferences' })
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc='LSP: [C]ode [A]ction' })

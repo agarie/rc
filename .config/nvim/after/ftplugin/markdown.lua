40fbbf51b2150844e1c6c585b9611ac6daed3f94
Carlos Agarie <carlos.agarie@gmail.com>
2026-02-23 16:42:48 -0300

    nvim: use TS for markdown folding

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,21 @@
-vim.g.markdown_fenced_languages = {'html', 'ruby', 'bash', 'zsh', 'python'}
+vim.g.markdown_fenced_languages = {'html', 'ruby', 'bash', 'zsh', 'python', 'js', 'ts' }
 
 vim.opt_local.foldlevel = 1
 
 vim.opt_local.shiftwidth = 4
 
-vim.opt_local.conceallevel = 0
+vim.opt_local.conceallevel = 1
+
+-- Taken from the nvim-treesitter documentation.
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'markdown' },
+    callback = function()
+      -- syntax highlighting, provided by Neovim
+      vim.treesitter.start()
+      -- folds, provided by Neovim
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo.foldmethod = 'expr'
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })

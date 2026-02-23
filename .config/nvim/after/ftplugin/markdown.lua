f771c9ad2e8f48282a5f94e533dee90b7fa04a83
Carlos Agarie <carlos.agarie@gmail.com>
2026-02-23 16:48:24 -0300

    nvim: fix markdown.lua TS usage
    Fenced languages list should contain the lang names instead of
    extensions, so `javascript` instead of `js`.
    
    I copy&pasted the snippet from the docs without thinking, so it was
    creating an autocommand when this file is already a `ftplugin`, i.e., it
    is executed only when `filetype=markdown`.

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,4 @@
-vim.g.markdown_fenced_languages = {'html', 'ruby', 'bash', 'zsh', 'python', 'js', 'ts' }
+vim.g.markdown_fenced_languages = {'html', 'ruby', 'bash', 'zsh', 'python', 'javascript', 'typescript' }
 
 vim.opt_local.foldlevel = 1
 
@@ -7,15 +7,12 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.conceallevel = 1
 
 -- Taken from the nvim-treesitter documentation.
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'markdown' },
-    callback = function()
-      -- syntax highlighting, provided by Neovim
-      vim.treesitter.start()
-      -- folds, provided by Neovim
-      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-      vim.wo.foldmethod = 'expr'
-      -- indentation, provided by nvim-treesitter
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end,
-  })
+-- syntax highlighting, provided by Neovim
+vim.treesitter.start()
+
+-- folds, provided by Neovim
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo.foldmethod = 'expr'
+
+-- indentation, provided by nvim-treesitter
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
--- .config/nvim/after/syntax/markdown.vim
+++
@@ -1,4 +0,0 @@
-" https://www.maero.dk/markdown-frontmatter-syntax-highlighting/
-unlet b:current_syntax
-syntax include @Yaml syntax/yaml.vim
-syntax region yamlFrontMatter start=/\%^---$/ end=/^---$/ keepend contains=@Yaml
